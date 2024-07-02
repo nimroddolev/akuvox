88bcc7a93ec5d9dd4313d568272c07e73a71babf
Nimrod Dolev <nimrod.dolev@gmail.com>
2024-07-02 15:13:01 +0300

    Fix for broken camera stream

--- custom_components/akuvox/camera.py
+++ custom_components/akuvox/camera.py
@@ -1,58 +1,75 @@
 """Camera platform for akuvox."""
-from homeassistant.components.generic.camera import GenericCamera
+
+from collections.abc import Callable, Awaitable
+from requests.auth import HTTPDigestAuth
+import requests
+
 from homeassistant.helpers import storage
 from homeassistant.helpers.entity import DeviceInfo
-from homeassistant.const import (
-    CONF_NAME,
-    CONF_VERIFY_SSL,
-    ATTR_MODEL,
-    ATTR_MANUFACTURER,
-    ATTR_IDENTIFIERS,
-)
+from homeassistant.const import ATTR_IDENTIFIERS, CONF_NAME, CONF_VERIFY_SSL
 from homeassistant.core import HomeAssistant
+from homeassistant.components.ffmpeg import get_ffmpeg_manager
+from homeassistant.components.camera import Camera
+from homeassistant.components.generic.camera import GenericCamera
 
-from .const import (
-    DOMAIN,
-    LOGGER,
-    NAME,
-    VERSION,
-    DATA_STORAGE_KEY
-)
+from .const import DOMAIN, LOGGER, NAME, VERSION, DATA_STORAGE_KEY
 
-async def async_setup_entry(hass: HomeAssistant, _entry, async_add_devices):
+
+async def async_setup_entry(hass: HomeAssistant,
+                            _entry,
+                            async_add_devices: Callable[[list], Awaitable[None]]):
     """Set up the camera platform."""
     store = storage.Store(hass, 1, DATA_STORAGE_KEY)
     device_data = await store.async_load()
-    cameras_data = device_data["camera_data"] # type: ignore
+
+    if not device_data:
+        LOGGER.error("No device data found")
+        return
+
+    cameras_data = device_data.get("camera_data")
+    if not cameras_data:
+        LOGGER.error("No camera data found in device data")
+        return
 
     entities = []
     for camera_data in cameras_data:
         name = str(camera_data["name"]).strip()
         rtsp_url = str(camera_data["video_url"]).strip()
         entities.append(AkuvoxCameraEntity(
+            hass=hass,
             name=name,
-            rtsp_url=rtsp_url))
-    async_add_devices(entities)
+            rtsp_url=rtsp_url
+        ))
 
+    if async_add_devices is None:
+        LOGGER.error("async_add_devices is None")
+        return
+
+    async_add_devices(entities)
+    return True
 
 class AkuvoxCameraEntity(GenericCamera):
     """Akuvox camera class."""
 
     def __init__(
         self,
+        hass: HomeAssistant,
         name: str,
-        rtsp_url: str,
-    ) -> None:
+        rtsp_url: str) -> None:
         """Initialize the Akuvox camera class."""
+        LOGGER.debug("Initializing Akuvox camera '%s'", name)
+
         super().__init__(
-            hass=self.hass,
+            hass=hass,
             device_info={
+                ATTR_IDENTIFIERS: {(DOMAIN, name)},
                 CONF_NAME: name,
                 "stream_source": rtsp_url,
                 "limit_refetch_to_url_change": True,
                 "framerate": 2,
                 "content_type": "",
                 CONF_VERIFY_SSL: False,
+                "rtsp_transport": "udp"
             },
             identifier=name,
             title=name,
@@ -60,15 +77,13 @@ class AkuvoxCameraEntity(GenericCamera):
 
         self._name = name
         self._rtsp_url = rtsp_url
-        self._rtsp_to_webrtc = True
-
         self._attr_unique_id = name
         self._attr_name = name
 
-        LOGGER.debug("Adding Akuvox camera '%s'", self._attr_unique_id)
         self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, name)},  # type: ignore
+            identifiers={(DOMAIN, name)},
             name=name,
             model=VERSION,
             manufacturer=NAME,
         )
+
